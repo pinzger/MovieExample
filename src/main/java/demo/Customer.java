86de5bc28f800e136abd82e3e2d2391d092ec37a
Martin Pinzger <martin.pinzger@aau.at>
2020-04-08 08:26:41 +0200

    replaced Vector with List

--- src/main/java/demo/Customer.java
+++ src/main/java/demo/Customer.java
@@ -1,62 +1,59 @@
 package demo;
 
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
-
     private String _name;
-    private Vector<Rental> _rentals = new Vector<Rental>();
-    
-    public Customer (String name) {
+    private List<Rental> _rentals = new ArrayList<Rental>();
+
+    public Customer(String name) {
         _name = name;
     }
-    
+
     public void addRental(Rental arg) {
-        _rentals.addElement(arg);
+        _rentals.add(arg);
     }
-    
+
     public String getName() {
         return _name;
     }
-    
+
     public String statement() {
         double totalAmount = 0;
-        String result = "Rental Record for " + getName() + "\n";
-        for (Rental each: _rentals) {
+        StringBuilder result = new StringBuilder("Rental Record for " + getName() + "\n");
+        for (Rental each : _rentals) {
             double thisAmount = 0;
-            
+
             // determine amounts for each line
             switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                thisAmount += 2;
-        		if (each.getDaysRented() > 2) {
-        			thisAmount += (each.getDaysRented() - 2) * 1.5;
-        		}
-                break;
-            
-            case Movie.NEW_RELEASE:
-                thisAmount = each.getDaysRented() * 3;
-            	break;
-            
-            case Movie.CHILDRENS:
-                thisAmount += 1.5;
-            	if (each.getDaysRented() > 3) {
-            	    thisAmount += (each.getDaysRented() - 3) * 1.5;
-            	}
-            	break;
-
-            default: break;
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2) {
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount = each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3) {
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    }
+                    break;
+                default:
+                    break;
             }
-            
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    thisAmount + "\n";
-            totalAmount += thisAmount;            
-        }
-        
-        // add footer lines
-        result += "Amount owed is " + totalAmount;
 
-        return result;
+            // show figures for this rental
+            result.append("\t").append(each.getMovie().getTitle()).append("\t").append(thisAmount).append("\n");
+            totalAmount += thisAmount;
+        }
+
+        // add footer lines
+        result.append("Amount owed is ").append(totalAmount);
+
+        return result.toString();
     }
 }
